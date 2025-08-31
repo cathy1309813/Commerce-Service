50eeda61039032f787ece70d2c7229b77649ce97
cathy1309813 <whityolanda@gmail.com>
2025-08-31 20:16:01 +0800

    Project Implementation 2025-08-31
    1.刪除不小心新增Class

--- src/main/java/com/gtalent/commerce/service/models/UserSegment.java
+++ src/main/java/com/gtalent/commerce/service/models/UserSegment.java
@@ -6,28 +6,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user_segments")
+@Table(
+        name = "user_segments",
+        //同一個 user 不能對應同一個 segment 多次
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "segment_id"}))
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-//users 和 segments 是多對多，中間由 user_segments 表格具體實現
-//user_segments 有額外欄位 has_newsletter
+/*
+ * users 和 segments 是多對多關聯，
+ * 中間由 user_segments 表格具體實現。
+ * user_segments 有額外欄位 has_newsletter
+ */
 public class UserSegment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    // 關聯到 User
-    @ManyToOne
+    //關聯到 User
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    // 關聯到 Segment
-    @ManyToOne
+    //關聯到 Segment
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "segment_id", nullable = false)
     private Segment segment;
-
-    // 額外欄位
-    @Column(name = "has_newsletter", nullable = false)
-    private boolean hasNewsletter = false;
 }
